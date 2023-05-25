85336a3d1c2dd3f73ff724510ab601ba686f5681
juhatae <juhatae@192.168.50.24>
2023-05-25 11:41:57 +0900

    다시수정

--- teamPractice/src/teamPractice/testclass.java
+++ teamPractice/src/teamPractice/testclass.java
@@ -1,9 +1,6 @@
 package teamPractice;
-<<<<<<< HEAD
 // 주하태
-=======
 // 장명근
->>>>>>> branch 'master' of https://github.com/jyk7813/teampractice.git
 public class testclass {
 	public static void main(String[] args) {
 		System.out.println("hello");
